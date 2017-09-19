4ce4e759c5f8d657bb8285ca0cb8e6c91fea8404
Sanket Achari <sanketachari@ufl.edu>
2017-09-19 11:01:59 -0400

    New project

---
+++ lib/gossip_simulator.ex
@@ -0,0 +1,18 @@
+defmodule GossipSimulator do
+  @moduledoc """
+  Documentation for GossipSimulator.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GossipSimulator.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
